83a85fad3bff3b3cb11363cbc30a46c3d3b66ff1
ozkansari <ozkansari@gmail.com>
2020-01-29 23:10:52 +0300

    web duzenlemeler

--- appticketz-web/src/main/java/net/javaci/sample/AppticketzBoot.java
+++ appticketz-web/src/main/java/net/javaci/sample/AppticketzBoot.java
@@ -3,25 +3,34 @@ package net.javaci.sample;
 import javax.faces.webapp.FacesServlet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.sun.faces.config.ConfigureListener;
 
-@SpringBootApplication
 @Configuration
-public class AppticketzBoot {
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+public class AppticketzBoot extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppticketzBoot.class, args);
 	}
 
-	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppticketzBoot.class);
+	}
+
 	@Bean
 	public ServletRegistrationBean<FacesServlet> facesServletRegistraiton() {
 		ServletRegistrationBean<FacesServlet> registration = new ServletRegistrationBean<FacesServlet>(
@@ -36,13 +45,13 @@ public class AppticketzBoot {
 	public ServletContextInitializer servletContextInitializer() {
 		return servletContext -> {
 			servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
-			// Primefacesin ücretsiz temalarından bootstrap örneği yaptık
-			// değiştirebilirsiniz
+			// Primefacesin ucretsiz temalarindan bootstrap ornegi yaptik
+			// degistirebilirsiniz
 			servletContext.setInitParameter("primefaces.THEME", "bootstrap");
-			// Primefaces client browser tarafında kontrol edilebilme örneğin textbox 10
-			// karakter olmalı vs..
+			// Primefaces client browser tarafinda kontrol edilebilme ornegin textbox 10
+			// karakter olmali vs..
 			servletContext.setInitParameter("primefaces.CLIENT_SIDE_VALIDATION", Boolean.TRUE.toString());
-			// Xhtml sayfalarında commentlerin parse edilmemesi.
+			// Xhtml sayfalarinda commentlerin parse edilmemesi.
 			servletContext.setInitParameter("javax.faces.FACELETS_SKIP_COMMENTS", Boolean.TRUE.toString());
 			// primefaces icon set için
 			servletContext.setInitParameter("primefaces.FONT_AWESOME", Boolean.TRUE.toString());
